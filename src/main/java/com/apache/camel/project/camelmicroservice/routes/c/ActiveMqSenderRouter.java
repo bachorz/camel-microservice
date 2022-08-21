b57aab62c661a527473cdefacce5f54827603956
rafal <rodzer116@gmail.com>
2022-08-21 21:25:18 +0200

    add conf with Kafka and RestApi

--- src/main/java/com/apache/camel/project/camelmicroservice/routes/c/ActiveMqSenderRouter.java
+++ src/main/java/com/apache/camel/project/camelmicroservice/routes/c/ActiveMqSenderRouter.java
@@ -8,9 +8,17 @@ public class ActiveMqSenderRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-      from("timer:active-mq-timer?period=10000")
-              .transform().constant("My message for Active MQ")
-              .log("${body}")
-              .to("activemq:my-queue");
+//      from("timer:active-mq-timer?period=10000")
+//              .transform().constant("My message for Active MQ")
+//              .log("${body}")
+//              .to("activemq:my-queue");
+
+//        from("file:files/json")
+//                .log("${body}")
+//                .to("activemq:my-queue");
+
+        from("file:files/xml")
+                .log("${body}")
+                .to("activemq:my-xml-queue");
     }
 }
