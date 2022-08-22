c72137fce6c22bc3d610712fd456a4376ae64643
rafal <rodzer116@gmail.com>
2022-08-22 12:04:07 +0200

    add logic to route by choice() and when()

--- src/main/java/com/apache/camel/project/camelmicroservice/routes/version/MyFileRouter.java
+++ src/main/java/com/apache/camel/project/camelmicroservice/routes/version/MyFileRouter.java
@@ -8,7 +8,17 @@ public class MyFileRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:files/input")
-                .log("${body}")
+                .routeId("Files-Input-Route")
+                .transform().body(String.class)
+                .choice()
+                    .when(simple("${file:ext} == 'xml' "))
+                        .log("XML FILE")
+                    .when(simple("${body} contains 'USD' "))
+                        .log("Not an XML FILE but contains USD")
+                    .otherwise()
+                        .log("Not an XML FILE")
+                .end()
+                .log("${body} path to file ${file:absolute.path}")
                 .to("file:files/output");
     }
 }
